e1b93f9bdecb37bbd23c387ee8de4b0ed8611620
ssmrrr <septr_4022@naver.com>
2023-09-21 17:30:04 +0900

    글쓰기 부분(미완성)

--- daangnproject/daangnproject/urls.py
+++ daangnproject/daangnproject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from daangn_app import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('daangn/', include('daangn_app.urls')),
 ]
